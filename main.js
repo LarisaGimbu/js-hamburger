d11a8b9e5196663ceadb4f54cb4a32baf9f82494
LarisaGimbu <gimbularisa@gmail.com>
2021-10-22 17:05:31 +0200

    hamburgerMenu-active

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const btnHamburger = document.querySelector('.fa-bars');
 
 btnHamburger.addEventListener('click',function(){
-  console.log('ho schiacciato l\'hamburger')
+  
+  hamburgerMenu.classList.add('active')
 })
