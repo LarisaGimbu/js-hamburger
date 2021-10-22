23873f0e87d23e8650d43c8d90ae6d8f6daf3346
LarisaGimbu <gimbularisa@gmail.com>
2021-10-22 17:09:30 +0200

    hamburgerMenu-remove-active

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 
 const btnHamburger = document.querySelector('.fa-bars');
+const btnCloseHamburger = document.querySelector('.close')
 
 btnHamburger.addEventListener('click',function(){
   
-  hamburgerMenu.classList.add('active')
+  hamburgerMenu.classList.add('active');
+})
+
+btnCloseHamburger.addEventListener('click',function(){
+
+  hamburgerMenu.classList.remove('active');
 })
