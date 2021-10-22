fb1edf3ac55843bdba5d7c10ec1c5d222a952328
LarisaGimbu <gimbularisa@gmail.com>
2021-10-22 16:56:33 +0200

    hamburger-click

---
+++ main.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+const btnHamburger = document.querySelector('.fa-bars');
+
+btnHamburger.addEventListener('click',function(){
+  console.log('ho schiacciato l\'hamburger')
+})
